26c9e7d13ecf6590c7511e50928718d90469cff6
Seth Kitchen <sjkyv5@mst.edu>
2021-01-06 00:11:40 -0600

    define combined as global

--- games/stocks.py
+++ games/stocks.py
@@ -242,6 +242,8 @@ class Stocks:
         return self.get_observation()
 
     def step(self, action):
+        global combined
+
         if action>=1 and action<=10:
             #Buying
             stockPrice = combined.iloc[self.stockLookedAt-1, self.day-1]
@@ -272,6 +274,8 @@ class Stocks:
         return self.get_observation(), reward, done
 
     def get_observation(self):
+        global combined
+
         # Width is [DayNumber, BuyingPowerForStock, CurrentAmountInvestedInStock, CurrentPrice] 
         self.observation_shape = (self.numberOfStocks, 1, 4)  # Dimensions of the game observation, must be 3D (channel, height, width). For a 1D array, please reshape it to (1, 1, length of array)
         allStocks = []
@@ -291,6 +295,8 @@ class Stocks:
         return (self.day == 5983)
 
     def render(self):
+        global combined
+        
         print('Day:', self.day, 'Buying Power:', self.buyingPower)
         for i in range(0, self.numberOfStocks):
             stockPrice = combined.iloc[i, self.day-1]
